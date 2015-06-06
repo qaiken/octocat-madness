886435f58a0b5c64c727c04be4f4c648f2d32246
Quinton Aiken <quintonlaiken@gmail.com>
2015-06-06 15:42:25 -0500

    make scene scroll duration responsive to html height

--- _/scripts/app.js
+++ _/scripts/app.js
@@ -6,11 +6,16 @@ function pathPrepare ($el) {
   $el.css("stroke-dashoffset", lineLength);
 }
 
+function getHtmlHeight() {
+  return $html.height();
+}
+
 var $outline = $("path#outline");
 var $leftEye = $("path#left-eye");
 var $rightEye = $("path#right-eye");
 var $nose = $("path#nose");
 var $mouth = $("path#mouth");
+var $html = $("html");
 
 // prepare SVG
 [$outline,$leftEye,$rightEye,$nose,$mouth].forEach(pathPrepare);
@@ -21,15 +26,19 @@ var controller = new ScrollMagic.Controller();
 // build tween
 var tween = new TimelineMax()
   .add(TweenMax.to($outline, 1, {strokeDashoffset: 0, ease:Linear.easeNone}),0)
-  .add(TweenMax.to($outline, 1, {fill: "#33629c", ease:Linear.easeNone}), 0)
+  .add(TweenMax.to($outline, 1, {fillOpacity: 1, ease:Linear.easeNone}), 0)
   .add(TweenMax.to($leftEye, 0.5, {strokeDashoffset: 0, ease:Linear.easeNone}),0.5)
   .add(TweenMax.to($rightEye, 0.5, {strokeDashoffset: 0, ease:Linear.easeNone}),0.5)
   .add(TweenMax.to($nose, 0.5, {strokeDashoffset: 0, ease:Linear.easeNone}),0.5)
   .add(TweenMax.to($mouth, 0.5, {strokeDashoffset: 0, ease:Linear.easeNone}),0.5)
-  .add(TweenMax.to("path", 1, {fill: "#33629c", ease:Linear.easeNone}), 0.5);
+  .add(TweenMax.to("path", 1, {fillOpacity: 1, ease:Linear.easeNone}), 0.5);
 
 // build scene
-var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 512, tweenChanges: true})
-        .setTween(tween)
-        .addIndicators() // add indicators (requires plugin)
-        .addTo(controller);
+var scene = new ScrollMagic.Scene({triggerElement: "body", triggerHook: "onLeave",duration: getHtmlHeight, tweenChanges: true})
+  .setTween(tween)
+  .addIndicators()
+  .addTo(controller);
+
+$(window).on('resize',function() {
+  scene.duration(getHtmlHeight);
+});
