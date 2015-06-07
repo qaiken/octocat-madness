e24b5b95a2c2bda908b70ddc1924b0d5451ae081
Quinton Aiken <quintonlaiken@gmail.com>
2015-06-07 12:39:40 -0500

    switch to module pattern and initOcto

--- _/scripts/app.js
+++ _/scripts/app.js
@@ -1,44 +1,64 @@
 var $ = require('jQuery');
 
-function pathPrepare ($el) {
-  var lineLength = $el[0].getTotalLength();
-  $el.css("stroke-dasharray", lineLength);
-  $el.css("stroke-dashoffset", lineLength);
-}
+var ScrollApp = (function() {
 
-function getHtmlHeight() {
-  return $html.height();
-}
+  function pathPrepare ($el) {
+    var lineLength = $el[0].getTotalLength();
+    $el.css("stroke-dasharray", lineLength);
+    $el.css("stroke-dashoffset", lineLength);
+  }
 
-var $outline = $("path#outline");
-var $leftEye = $("path#left-eye");
-var $rightEye = $("path#right-eye");
-var $nose = $("path#nose");
-var $mouth = $("path#mouth");
-var $html = $("html");
+  function randomNumber(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+  }
 
-// prepare SVG
-[$outline,$leftEye,$rightEye,$nose,$mouth].forEach(pathPrepare);
+  function octoScrollEnd() {
+    return $html.height() - $win.height() - ($html.height() / numJumpers);
+  }
 
-// init controller
-var controller = new ScrollMagic.Controller();
+  function initOcto() {
 
-// build tween
-var tween = new TimelineMax()
-  .add(TweenMax.to($outline, 1, {strokeDashoffset: 0, ease:Linear.easeNone}),0)
-  .add(TweenMax.to($outline, 1, {fillOpacity: 1, ease:Linear.easeNone}), 0)
-  .add(TweenMax.to($leftEye, 0.5, {strokeDashoffset: 0, ease:Linear.easeNone}),0.5)
-  .add(TweenMax.to($rightEye, 0.5, {strokeDashoffset: 0, ease:Linear.easeNone}),0.5)
-  .add(TweenMax.to($nose, 0.5, {strokeDashoffset: 0, ease:Linear.easeNone}),0.5)
-  .add(TweenMax.to($mouth, 0.5, {strokeDashoffset: 0, ease:Linear.easeNone}),0.5)
-  .add(TweenMax.to("path", 1, {fillOpacity: 1, ease:Linear.easeNone}), 0.5);
+    [$outline,$leftEye,$rightEye,$nose,$mouth].forEach(pathPrepare);
+    
+    var tween = new TimelineMax()
+      .add(TweenMax.to("path", 0.1, {strokeOpacity: 1, ease:Linear.easeNone}))
+      .add(TweenMax.to($outline, 1, {strokeDashoffset: 0, ease:Linear.easeNone}),0)
+      .add(TweenMax.to($leftEye, 0.5, {strokeDashoffset: 0, ease:Linear.easeNone}),0.5)
+      .add(TweenMax.to($rightEye, 0.5, {strokeDashoffset: 0, ease:Linear.easeNone}),0.5)
+      .add(TweenMax.to($nose, 0.5, {strokeDashoffset: 0, ease:Linear.easeNone}),0.5)
+      .add(TweenMax.to($mouth, 0.5, {strokeDashoffset: 0, ease:Linear.easeNone}),0.5)
+      .add(TweenMax.to("path", 1, {fillOpacity: 1, ease:Linear.easeNone}), 0.5);
 
-// build scene
-var scene = new ScrollMagic.Scene({triggerElement: "body", triggerHook: "onLeave",duration: getHtmlHeight, tweenChanges: true})
-  .setTween(tween)
-  .addIndicators()
-  .addTo(controller);
+    var octoCat = new ScrollMagic.Scene({triggerElement: "body", triggerHook: "onLeave",duration: octoScrollEnd(), tweenChanges: true})
+      .setTween(tween)
+      // .addIndicators()
+      .addTo(controller);
 
-$(window).on('resize',function() {
-  scene.duration(getHtmlHeight);
-});
+    $win.on('resize',function() {
+      octoCat.duration(octoScrollEnd);
+    });
+  }
+
+  function init() {
+    initOcto();
+  }
+
+  var timingFunctions = ['ease','ease-in','ease-out','ease-in-out','linear'];
+  var $outline = $("path#outline");
+  var $leftEye = $("path#left-eye");
+  var $rightEye = $("path#right-eye");
+  var $nose = $("path#nose");
+  var $mouth = $("path#mouth");
+  var $html = $("html");
+  var $win = $(window);
+  var $body = $('body');
+  var controller = new ScrollMagic.Controller();
+  var numJumpers = 40;
+
+  return {
+    init: init
+  };
+
+}());
+
+ScrollApp.init();
